5a1525adb12a8701c10cacb5198ad4db7995e01b
wscoder-max <nuzriz09@gmail.com>
2026-01-28 18:38:20 +0000

    Tuple Operations

---
+++ Class9.py
@@ -0,0 +1,19 @@
+tuple_x = ("tuple", False, 3.2, 1)
+print(tuple_x)
+
+tuplex = (4, 6, 2, 8, 3, 1)
+print(tuplex)
+
+tuplex += (9,)
+print(tuplex)
+
+tuple1 = (50, 10, 60, 70, 50)
+print(tuple1.count(50))
+
+tupley = (2, 4, 3, 5, 4, 6, 7, 8, 6, 1)
+
+slice = tupley[3:5]
+print(slice)
+
+slice2 = tupley[:6]
+print(slice2)
