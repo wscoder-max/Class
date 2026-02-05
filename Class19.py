52ce80b56640caf08ba3dec4ccd9e514dda35d32
wscoder-max <nuzriz09@gmail.com>
2026-02-05 18:56:15 +0000

    Symmetric Difference of Sets

---
+++ Class19.py
@@ -0,0 +1,7 @@
+set1 = {"blue", "green"}
+set2 = {"yellow", "blue"}
+print("Symmetric difference of set1 and set2: ", set1.symmetric_difference(set2))
+
+set3 = {1, 2, 3, 4, 5}
+set4 = {1, 5, 6, 7, 8, 9}
+print("Symmetric difference of set3 and set4: ", set3.symmetric_difference(set4))
