497d180a0f0f106e38642318543183bfa1278d31
wscoder-max <nuzriz09@gmail.com>
2026-02-05 17:48:53 +0000

    Get the Country Code

---
+++ Class15.py
@@ -0,0 +1,7 @@
+SPCMcountry_code = {'India': '0091', 'Australia': '0025', 'Nepal': '00977'}
+
+print("Country code for India -")
+print(SPCMcountry_code.get('India', 'Not Found'))
+ 
+print("Country code for Japan -")
+print(SPCMcountry_code.get('Japan', 'Not Found'))
