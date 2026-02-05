3e7b9a79bba00aa6aada73935501f403098b6672
wscoder-max <nuzriz09@gmail.com>
2026-02-05 18:50:05 +0000

    Set Intersection

---
+++ Class17.py
@@ -0,0 +1,10 @@
+setx = {"green", "blue"}
+sety = {"blue", "yellow"}
+
+print("Original set elements: ")
+print(setx)
+print(sety)
+
+print("\nIntersection of the two given sets: ")
+setz = setx.intersection(sety)
+print(setz)
