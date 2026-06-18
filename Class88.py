a92c6a823ff58095cd830816cd736a51cc2d2d94
wscoder-max <nuzriz09@gmail.com>
2026-06-18 20:06:17 +0100

    Roman To Integer

---
+++ Class88.py
@@ -0,0 +1,13 @@
+def romanToInt(romanInput):
+    roman_numerals = {'M': 1000, 'D': 500 , 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    resultInteger = 0
+ 
+    for i in range(0, len(romanInput) - 1):
+        if roman_numerals[romanInput[i]] < roman_numerals[romanInput[i + 1]]:
+            resultInteger -= roman_numerals[romanInput[i]]
+        else:
+            resultInteger += roman_numerals[romanInput[i]]      
+    return resultInteger + roman_numerals[romanInput[-1]]
+ 
+roman = input("Input roman numeral: ")
+print(f"Arabic Numeral: {romanToInt(roman)}")
