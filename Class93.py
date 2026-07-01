a5770dcaa86a22bac502ec5cfe8ac793e676be3d
wscoder-max <nuzriz09@gmail.com>
2026-07-01 19:33:20 +0100

    Prime Numbers

---
+++ Class93.py
@@ -0,0 +1,10 @@
+from math import sqrt
+
+number = int(input("Enter a number: "))
+
+for i in range(2, int(sqrt(number)) + 1):
+    if number % i == 0:
+        print(f"{number} is not a prime number.")
+        break
+else:
+    print(f"{number} is a prime number.")
